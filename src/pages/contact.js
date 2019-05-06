fe45be610736d72e9e869aa013d461d5202f3a21
Changyang Zhang <changyang.zhang@accedo.tv>
2019-05-06 16:22:59 +0200

    Incase js cannot be loaded

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,15 +13,14 @@ const ContactPage = () => {
                 <Helmet><script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer/></Helmet>
                 <h1>Get in touch</h1>
                 <div className={contactStyles.contact}>
-                    <a className={contactStyles.box} href="mailto:changyang629@gmail.com"><p className={contactStyles.text}>Message me</p></a>
+                    <a className={[contactStyles.link, contactStyles.box].join(' ')} href="mailto:changyang629@gmail.com"><p className={contactStyles.text}>Message me</p></a>
                     <div className="LI-profile-badge" data-version="v1" data-size="medium" data-locale="en_US"
                          data-type="horizontal" data-theme="dark" data-vanity="changyangzhang"><a
-                        className="LI-simple-link" href='https://se.linkedin.com/in/changyangzhang?trk=profile-badge'>Changyang
-                        Z.</a></div>
+                        className={["LI-simple-link", contactStyles.link].join(' ')} href='https://se.linkedin.com/in/changyangzhang?trk=profile-badge' target="_blank" rel="noopener noreferrer">
+                        FInd me on LinkedIn.</a></div>
                 </div>
             </Layout>
         </div>
     )
 };
-
 export default ContactPage
